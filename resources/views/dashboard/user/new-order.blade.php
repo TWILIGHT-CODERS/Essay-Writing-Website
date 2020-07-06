0e7b80cdcd4a0b69f24108cc69f37637cbb15b64
Johnson Matoke <johnsonmatoke@gmail.com>
2020-07-07 00:22:21 +0300

    Quick Fix -> added blog folder

--- resources/views/dashboard/user/new-order.blade.php
+++ resources/views/dashboard/user/new-order.blade.php
@@ -8,7 +8,18 @@
                 <div class="card-header"> New Order</div>
                 <div class="card-body">
                   <div class="row">
-                    add content here
+                    <div class="col-12">
+                      <form class="formTest" method="post" action="/test">
+                        @csrf
+                        <div class="form-group col-sm-6">
+                          <label for="test">Name</label>
+                          <input type="text" name="test" value="" class="form-control">
+                        </div>
+                        <div class="form-group col-sm-6">
+                          <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                      </form>
+                    </div>
                   </div>
                 </div>
               </div>
